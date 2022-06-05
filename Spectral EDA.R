4346dce216dc7cd6943aa411d388cadbcaafd2c6
pradyutnair <p.nair@student.tue.nl>
2022-06-05 22:55:28 +0200

    Conducted spectral smoothing

--- Spectral EDA.R
+++ Spectral EDA.R
@@ -1,5 +1,5 @@
 library(pacman)
-pacman::p_load(ggplot2, Hmisc, dplyr, squash,superml,pavo)
+pacman::p_load(ggplot2, Hmisc, dplyr, squash, pavo,plyr)
 
 df <- data.frame(read.csv('./Chicken Fillet NIR data.csv',
                           strip.white = TRUE,
@@ -8,72 +8,98 @@ df <- data.frame(read.csv('./Chicken Fillet NIR data.csv',
 
 # Remove row 455 as it is most likely a measurement error
 df <- df[-c(455),]
-fresh <- df[df$Freshness == "FR",][5:length(df)]
-thawed <- df[df$Freshness == "TH",][5:length(df)]
+fresh <- df[df$Freshness == "FR",][,5:length(df)]
+thawed <- df[df$Freshness == "TH",][,5:length(df)]
 
 # Create list of wavelengths from column names
 wavelengths <- substring(colnames(df[5:length(df)]),2,8)
 wavelengths <- as.numeric(wavelengths)
 
-##############################################
-# Plotting the spectra for fresh chicken
-om <- t(df[(df$Freshness=="FR" & df$Scan_type=="OM"),][5:length(df)])
-tb <- t(df[(df$Freshness=="FR" & df$Scan_type=="TB"),][5:length(df)])
-tp <- t(df[(df$Freshness=="FR" & df$Scan_type=="TP"),][5:length(df)])
+###############################################
+# Function for creating spectral data
+create.spectra <- function(df,type){
+  # input is a word indicating whether we want fresh or thawed data,
+  # divide data by Scan_Type
+  if (type=="TH"){
+    om <- t(df[(df$Freshness=="TH" & df$Scan_type=="OM"),][5:length(df)])
+    tb <- t(df[(df$Freshness=="TH" & df$Scan_type=="TB"),][5:length(df)])
+    tp <- t(df[(df$Freshness=="TH" & df$Scan_type=="TP"),][5:length(df)])
+  }else{
+    om <- t(df[(df$Freshness=="FR" & df$Scan_type=="OM"),][5:length(df)])
+    tb <- t(df[(df$Freshness=="FR" & df$Scan_type=="TB"),][5:length(df)])
+    tp <- t(df[(df$Freshness=="FR" & df$Scan_type=="TP"),][5:length(df)])
+  }
+
+  # rename the first column to wl
+  colnames(om)[0] <- "wl"
+  colnames(tb)[0] <- "wl"
+  colnames(tp)[0] <- "wl"
+
+  # convert to spectral data using pavo package
+  om.spec <- as.rspec(om)
+  tb.spec <- as.rspec(tb)
+  tp.spec <- as.rspec(tp)
+
+  # change index column to wavelengths
+  om.spec$wl <-wavelengths
+  tb.spec$wl <-wavelengths
+  tp.spec$wl <-wavelengths
+
+  # return list of all spectral scan type data
+  spectra_data <- list("om"=om.spec,"tb"=tb.spec,"tp"=tp.spec,check.rows = TRUE)
+  return (spectra_data)
+
+}
+
+# Function for plotting spectral data
+plot.spectra <- function(om.spec,tb.spec,tp.spec,title,colors){
+  # Plot spectral data sequentially
+  plot(om.spec, xlab="",ylab="",col =colors[1],
+       ylim=c(0.5,3.5))
+  par(new=TRUE)
+  plot(tp.spec, xlab="",ylab="",col =colors[3],
+       ylim=c(0.5,3.5))
+  par(new=TRUE)
+  plot(tb.spec, xlab="Wavelength (nm)",ylab="Absorbance", col =colors[2],main=title,
+       ylim=c(0.5,3.5))
+  legend(x="topleft", legend=c("OM", "TB","TP"),
+         col=c(colors[1],colors[2],colors[3]), lty=1, cex=0.8)
+}
+
+################################################
+# Create spectral data for fresh and thawed data
+om.f <- create.spectra(df,"FR")$om
+tb.f <- create.spectra(df,"FR")$tb
+tp.f <- create.spectra(df,"FR")$tp
+
+om.t <- create.spectra(df,"TH")$om
+tb.t <- create.spectra(df,"TH")$tb
+tp.t <- create.spectra(df,"TH")$tp
+
+# Plot spectral data
+plot.spectra(om.f,tb.f,tp.f,title="Raw NIR data of fresh chicken fillets",
+             colors=c("blue","red","green"))
+plot.spectra(om.t,tb.t,tp.t,title="Raw NIR data of thawed chicken fillets",
+             colors=c("cyan","black","magenta"))
+
+###############################################
+# Performing spectral smoothing
+
+om.f.smooth <- procspec(om.f,opt="smooth",fixneg="none")
+tb.f.smooth <- procspec(tb.f,opt="smooth",fixneg="none")
+tp.f.smooth <- procspec(tp.f,opt="smooth",fixneg="none")
+
+om.t.smooth <- procspec(om.t,opt="smooth",fixneg="none")
+tb.t.smooth <- procspec(tb.t,opt="smooth",fixneg="none")
+tp.t.smooth <- procspec(tp.t,opt="smooth",fixneg="none")
+
+plot.spectra(om.f.smooth,tb.f.smooth,tp.f.smooth,title="Smoothed NIR data of fresh chicken fillets",
+             colors=c("blue","red","green"))
+plot.spectra(om.t.smooth,tb.t.smooth,tp.t.smooth,title="Smoothed NIR data of thawed chicken fillets",
+                colors=c("cyan","black","magenta"))
 
 
-colnames(om)[0] <- "wl"
-colnames(tb)[0] <- "wl"
-colnames(tp)[0] <- "wl"
-
-om.spec <- as.rspec(om)
-tb.spec <- as.rspec(tb)
-tp.spec <- as.rspec(tp)
-
-om.spec$wl <-wavelengths
-tb.spec$wl <-wavelengths
-tp.spec$wl <-wavelengths
-
-plot(om.spec, xlab="",ylab="",col ="Blue",
-     ylim=c(0.5,3.5))
-par(new=TRUE)
-plot(tp.spec, xlab="",ylab="",col ="Green",
-     ylim=c(0.5,3.5))
-par(new=TRUE)
-plot(tb.spec, xlab="Wavelength (nm)",ylab="Absorbance", col ="Red",main="Raw NIR data of fresh chicken fillets"
-  ,ylim=c(0.5,3.5))
-legend(x="topleft", legend=c("OM", "TB","TP"),
-       col=c("blue", "red","green"), lty=1, cex=0.8)
 
 
-##############################################
-# Plotting the spectra for thawed chicken
-om <- t(df[(df$Freshness=="TH" & df$Scan_type=="OM"),][5:length(df)])
-tb <- t(df[(df$Freshness=="TH" & df$Scan_type=="TB"),][5:length(df)])
-tp <- t(df[(df$Freshness=="TH" & df$Scan_type=="TP"),][5:length(df)])
 
 
-colnames(om)[0] <- "wl"
-colnames(tb)[0] <- "wl"
-colnames(tp)[0] <- "wl"
-
-om.spec <- as.rspec(om)
-tb.spec <- as.rspec(tb)
-tp.spec <- as.rspec(tp)
-
-om.spec$wl <-wavelengths
-tb.spec$wl <-wavelengths
-tp.spec$wl <-wavelengths
-
-plot(om.spec, xlab="",ylab="",col ="cyan",
-     ylim=c(0.5,3.5))
-par(new=TRUE)
-plot(tp.spec, xlab="",ylab="",col ="magenta",
-     ylim=c(0.5,3.5))
-par(new=TRUE)
-plot(tb.spec, xlab="Wavelength (nm)",ylab="Absorbance", col ="black",main="Raw NIR data of Thawed chicken fillets"
-  ,ylim=c(0.5,3.5))
-legend(x="topleft", legend=c("OM", "TB","TP"),
-       col=c("cyan", "black","magenta"), lty=1, cex=0.8)
-
-##############################################
