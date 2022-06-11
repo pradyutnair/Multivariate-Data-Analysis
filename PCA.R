4d3a7a06761293dd2657cc59030524b6a9ae6f68
pradyutnair <p.nair@student.tue.nl>
2022-06-11 19:43:24 +0200

    PCA

---
+++ PCA.R
@@ -0,0 +1,98 @@
+library(pacman)
+pacman::p_load(dplyr,MASS,ggplot2,openxlsx, tidyverse, plotly,ggpubr,reshape2,Hmisc,
+               cowplot, PerformanceAnalytics,signal)
+
+# Import the data and create dataframe
+setwd('./')
+df <- data.frame(read.csv('../GitHub/Chicken Fillet NIR data.csv',
+                          strip.white = TRUE,
+                          sep=';',
+                          stringsAsFactors = TRUE))
+# Remove row 455 as it is most likely a measurement error
+df <- df[-455,]
+fresh <- df[df$Freshness == "FR",]
+thawed <- df[df$Freshness == "TH",]
+
+# PCA of entire df
+df.pca <- prcomp(df[,5:length(df)], center=TRUE, scale = TRUE)
+summary(df.pca)
+fviz_eig(df.pca, addlabels = TRUE, ylim = c(0, 80))
+ind <- get_pca_ind(df.pca)
+
+groups <- as.factor(df$Scan_type)
+
+# Plot individual variables in the principal component axes
+ind.p <- fviz_pca_ind(df.pca, geom = "point", col.ind = df$Scan_type)
+ggpubr::ggpar(ind.p,
+              title = "PCA-Individuals",
+              subtitle = "Chicken NIR",
+              xlab = "PC1", ylab = "PC2",
+              legend.title = "Scan_Type", legend.position = "top",
+              ggtheme = theme_minimal(), palette="npg"
+)
+
+# Plot the cos2 of each PCA variable (wavelengths)
+fviz_pca_var(df.pca, col.var = "cos2",
+             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+             repel = TRUE # Avoid text overlapping
+)
+
+# Contributions of wavelengths to PC1
+fviz_contrib(df.pca, choice = "var", axes = 1, top = 50,fill="blue",col="black")
+# Contributions of wavelengths to PC2
+fviz_contrib(df.pca, choice = "var", axes = 2, top = 50,fill="orange",col="black")
+
+# Use PCA transformed data on a Random Forest Classifier
+
+# Use a 70-30 split and create train and test sets. Set  target vars as factors
+sample <- sample.split(df$Scan_type, SplitRatio = 0.7)
+train  <- subset(df, sample == TRUE)
+test   <- subset(df, sample == FALSE)
+
+
+X_train_base <- as.matrix(train[,5:length(train)])
+X_test_base <- as.matrix(test[,5:length(test)])
+y_train <- as.factor(train$Scan_type)
+y_test <- as.factor(test$Scan_type)
+
+# Remove first 4 columns from train and test sets
+train <- train[,5:length(train)]
+test <- test[,5:length(test)]
+
+# Convert to pca data
+train.pca <- prcomp(train, center = TRUE, scale. = TRUE)
+test.pca <- prcomp(test, center = TRUE, scale. = TRUE)
+
+# Use the first two components as training data
+train.pca <- train.pca$x[,1:2]
+test.pca <- test.pca$x[,1:2]
+
+# Instatiate the model on pca transformed data. Set number of trees = 2
+rf_base <- randomForest(y_train~.,data=X_train_base, ntree = 2)
+rf_pca <- randomForest(y_train~., data=train.pca, nTree=2)
+
+print(rf_base)
+print(rf_pca)
+
+# Plot feature importances
+varImpPlot(rf_base,main="Feature Importances of RF Classifier on base data")
+varImpPlot(rf_pca,main="Feature Importances of RF Classifier on PCA transformed data")
+
+# Create prediction variables for train and test data
+pred_base_train <- predict(rf_base,newdata=X_train_base,type='class')
+pred_base_test <- predict(rf_base,newdata=X_test_base,type='class')
+
+pred_pca_train <- predict(rf_pca,type='class')
+pred_pca_test <- predict(rf_pca,newdata=test.pca,type='class')
+
+# Return predictions
+print(paste0("RF accuracy on base train set: ",
+             round(mean(pred_base_train==y_train),3)))
+print(paste0("RF Accuracy on base test set: ",
+             round(mean(pred_base_test==y_test),3)))
+
+print(paste0("RF accuracy on PCA train set: ",
+             round(mean(pred_pca_train==y_train),3)))
+print(paste0("RF Accuracy on PCA test set: ",
+             round(mean(pred_pca_test==y_test),3)))
+
--- README.md
+++ README.md
@@ -4,3 +4,11 @@
 LDA on the is tested on the entire dataset by splitting a 70-30 ratio of train and test set. We want to analyse whether there is a discrepancy between class separation of _Scan Type_ when the data is smoothed or not.
 For the smoothing, a Savitsky-Golay algorithm was applied to the NIR data with `p=2 and n=3`, indicating that the where `p` is the filter order and `n` is the filter length.
 This hypothesis doesn't lead to conclusive results as the LDA model performs exactly the same for both datasets. The onyl difference is the slight variance in the visualisation of the linear discriminants of the smoothed data which have lower variance.
+# PCA
+Principal Component Analysis aims to describe maximum variation in the data. When conducted for the entire dataset, PC1 explains 62.4% of the variance, whereas PC2 explains 36.6% of the total variance. However, the data seems more separable in LDA. We also plot the cos2, which is a quality of representation of the variables.
+A high cos2 indicates a good representation of the variable on the principal component. In this case the variable is positioned close to the circumference of the correlation circle.
+The contribution of the top 50 wavelengths are also plotted for Principal Components 1 and 2.
+
+# PCA with RandomForestClassifier
+A RandomForestClassifier was used to train the model on the entire dataset. This was taken as the baseline performance. The number of trees was set to 2 and the baseline performance was quite good with 92.4% and 80% accuracy on the train and test sets respectively.
+However, with by using the first two principal components as the training data, we get a lower train accuracy of 84.6% but much better generalisation with a test accuracy of 85.3%.
