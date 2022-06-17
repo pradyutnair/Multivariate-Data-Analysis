72276a9e4077c41288e7917e473f5e4b292dc891
pradyutnair <p.nair@student.tue.nl>
2022-06-17 10:21:26 +0200

    SVM

--- SVM Tuning.R
+++ SVM Tuning.R
@@ -7,40 +7,94 @@ df <- read.csv('./NIR_SNV_MSC.csv',sep=',',stringsAsFactors=TRUE,strip.white = T
 df <- df[,-1]
 
 ############################################################
-# Split data into train test and validation sets
-base_sample <- partition(df$Freshness, p = c(train = 0.6, valid = 0.2, test = 0.2))
+# Split data into train and test sets
+base_sample <- partition(df$Freshness, p = c(train = 0.7, test = 0.3))
 base_train <- df[base_sample$train, ]
 base_valid <- df[base_sample$valid, ]
 base_test <- df[base_sample$test, ]
-?tune.svm
+
 ############################################################
-# Create baseline model
-m <- tune.svm(x = train.pca,y=y_train,,
-              tunecontrol=tune.control(cross=5),cost=1:3,gamma=seq(0,1,by=0.1))
+# Train set
+X_om_train <- base_train[base_train$Scan_type == "OM",][,5:length(base_train)]
+y_om_train <- base_train[base_train$Scan_type == "OM",]$Freshness
+
+X_tb_train <- base_train[base_train$Scan_type == "TB",][,5:length(base_train)]
+y_tb_train <- base_train[base_train$Scan_type == "TB",]$Freshness
+
+X_tp_train <- base_train[base_train$Scan_type == "TP",][,5:length(base_train)]
+y_tp_train <- base_train[base_train$Scan_type == "TP",]$Freshness
+
+# Test set
+X_om_test <- base_test[base_test$Scan_type == "OM",][,5:length(base_test)]
+X_tb_test <- base_test[base_test$Scan_type == "TB",][,5:length(base_test)]
+X_tp_test <- base_test[base_test$Scan_type == "TP",][,5:length(base_test)]
+
+y_om_test <- base_test[base_test$Scan_type == "OM",]$Freshness
+y_tb_test <- base_test[base_test$Scan_type == "TB",]$Freshness
+y_tp_test <- base_test[base_test$Scan_type == "TP",]$Freshness
+############################################################
+# SVM on OM
+m <- tune.svm(x = X_om_train,y=y_om_train,
+              tunecontrol=tune.control(cross=10),cost=1:3,gamma=seq(0,1,by=0.1))
 
 attributes(m)
-m$best.parameters
-m$best.model
 m$performances
-############################################################
-# Conduct PCA on data
-df.pca <- prcomp(df[,5:length(df)], center=TRUE, scale = TRUE)
-summary(df.pca)
-fviz_eig(df.pca, addlabels = TRUE, ylim = c(0, 80))
-ind <- get_pca_ind(df.pca)
-groups <- as.factor(df$Freshness)
+m$best.model
+m$best.parameters
 
-# Plot individual variables in the principal component axes
-ind.p <- fviz_pca_ind(df.pca, geom = "point", col.ind = df$Scan_type)
-ggpubr::ggpar(ind.p,
-              title = "PCA-Individuals",
-              subtitle = "Chicken NIR",
-              xlab = "PC1", ylab = "PC2",
-              legend.title = "Scan_type", legend.position = "top",
-              ggtheme = theme_minimal(), palette="npg")
+svm.om<- svm(formula = y_om_train ~ .,
+             data = data.frame(X_om_train),
+             gamma=m$best.parameters$gamma,
+             cost=m$best.parameters$cost,
+             type = 'C-classification',
+             kernel = 'radial')
 
+om_train_preds <- predict(svm.om,newdata=data.frame(X_om_train))
+om_test_preds <- predict(svm.om,newdata=data.frame(X_om_test))
+print(paste0("SVM accuracy on OM train set: ",
+             round(mean(om_train_preds==y_om_train),3)))
+print(paste0("SVM accuracy on OM test set: ",
+             round(mean(om_test_preds==y_om_test),3)))
 ############################################################
-# Conduct PCA on train set
-df.pca.train <- prcomp(base_train[,5:length(base_train)], center=TRUE, scale = TRUE)
-df.pca.valid <- prcomp(base_valid[,5:length(base_valid)], center=TRUE, scale = TRUE)
-fviz_eig(df.pca.valid, addlabels = TRUE, ylim = c(0, 80))
+# SVM on TB
+m <- tune.svm(x = X_tb_train,y=y_tb_train,
+              tunecontrol=tune.control(cross=10),cost=1:3,gamma=seq(0,1,by=0.1))
+
+m$best.model
+m$best.parameters
+
+svm.tb <- svm(formula = y_tb_train ~ .,
+             data = data.frame(X_tb_train),
+              gamma=m$best.parameters$gamma,
+              cost=m$best.parameters$cost,
+             type = 'C-classification',
+             kernel = 'radial')
+
+tb_train_preds <- predict(svm.tb,newdata=data.frame(X_tb_train))
+tb_test_preds <- predict(svm.tb,newdata=data.frame(X_tb_test))
+print(paste0("SVM accuracy on TB train set: ",
+             round(mean(tb_train_preds==y_tb_train),3)))
+print(paste0("SVM accuracy on TB test set: ",
+             round(mean(tb_test_preds==y_tb_test),3)))
+############################################################
+# SVM on TP
+m <- tune.svm(x = X_tp_train,y=y_tp_train,
+              tunecontrol=tune.control(cross=10),cost=1:3,gamma=seq(0,1,by=0.1))
+
+m$best.model
+m$best.parameters
+
+svm.tp <- svm(formula = y_tp_train ~ .,
+             data = data.frame(X_tp_train),
+             gamma=m$best.parameters$gamma,
+             cost=m$best.parameters$cost,
+             type = 'C-classification',
+             kernel = 'radial')
+
+tp_train_preds <- predict(svm.tp,newdata=data.frame(X_tp_train))
+tp_test_preds <- predict(svm.tp,newdata=data.frame(X_tp_test))
+print(paste0("SVM accuracy on TP train set: ",
+             round(mean(tp_train_preds==y_tp_train),3)))
+print(paste0("SVM accuracy on TP test set: ",
+                round(mean(tp_test_preds==y_tp_test),3)))
+############################################################
