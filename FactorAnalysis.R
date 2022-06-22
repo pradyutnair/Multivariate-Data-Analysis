0a8102f3d229075adcbb89b4d27b4923dc64e01e
GerbrUh <106966673+GerbrUh@users.noreply.github.com>
2022-06-22 13:17:39 +0200

    Update FactorAnalysis.R

--- FactorAnalysis.R
+++ FactorAnalysis.R
@@ -1,5 +1,5 @@
-NIR <- NIR_Preprocessed
-NIR <- NIR_Preprocessed
+NIR <- read.csv(file = 'NIR_Preprocessed.csv')
+NIR
 
 library(psych)
 library(corrplot)
@@ -11,7 +11,9 @@ datamatrix <- cor(NIR[6:130])
 corrplot(datamatrix, method="number")
 
 X <- NIR[6:130]
+X
 Y <- NIR[2:5]
+Y
 
 KMO(r=cor(X))
 cortest.bartlett(X)
@@ -32,9 +34,8 @@ ggplot(scree, aes(x = Factor_n, y = Eigenvalue, group = 1)) +
 
 parallel <- fa.parallel(X)
 
-
 fa.none <- fa(r=X, 
-              nfactors = 7, 
+              nfactors = 8, 
               # covar = FALSE, SMC = TRUE,
               fm="pa", # type of factor analysis we want to use ("pa" is principal axis factoring)
               max.iter=100, # (50 is the default, but we have changed it to 100
@@ -47,24 +48,9 @@ head(fa.none$scores)
 
 
 Y <- NIR[2:5]
-#Y["Production_system"][Y["Production_system"]=="1 Star"] <- 10
-#Y["Production_system"][Y["Production_system"]=="2 stars"] <- 20
-#Y["Production_system"][Y["Production_system"]=="CONV"] <- 30
-#Y["Production_system"][Y["Production_system"]=="ORG"] <- 40
-#Y["Production_system"][Y["Production_system"]=="STD"] <- 50
-#Y["Production_system"][Y["Production_system"]=="FR"] <- 60
-#Y["Production_system"][Y["Production_system"]=="CF"] <- 70
-#Y["Production_system"][Y["Production_system"]=="MAR"] <- 80
 
 Y["Freshness"][Y["Freshness"]=="FR"] <- 0
 Y["Freshness"][Y["Freshness"]=="TH"] <- 1
-
-#Y
-#rapply(num_Ps, function(x) func(x), how = "replace")
-
-#Y["Production_system"]
-#Y["Freshness"]
-#fa.none$scores[,"PA3"]
 
 regdata <- data.frame (first_column  = Y["Freshness"], 
                   PA3 = fa.none$scores[,"PA3"],
@@ -73,14 +59,11 @@ regdata <- data.frame (first_column  = Y["Freshness"],
                   PA4 = fa.none$scores[,"PA4"],
                   PA5 = fa.none$scores[,"PA5"],
                   PA6 = fa.none$scores[,"PA6"],
-                  PA7 = fa.none$scores[,"PA7"]
+                  PA7 = fa.none$scores[,"PA7"],
+                  PA8 = fa.none$scores[,"PA8"]
 )
 regdata
 
-#regdata <- cbind(Y$Production_system, fa.none$scores)
-#Labeling the data
-#names(regdata) <- c("Production_system", "F1", "F2",
- #                    "F3", "F4", "F5", "F6")
 head(regdata)
 
 set.seed(100)
@@ -98,4 +81,4 @@ pred_test
 test$Freshness_pred <- round(pred_test, digits = 0)#signif(pred_test, digits = 0)#round(pred_test, digits = 0)
 head(test[c("Freshness","Freshness_pred")], 100)
 table(test[,"Freshness"] == test[, "Freshness_pred"])
-738/(478+738)
+725/(491+725)
